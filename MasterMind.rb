985b0de9ff345f65c1a76e2aabbd98f46a5d81dd
rulerpe <neetisthebest@gmail.com>
2014-11-06 17:20:02 -0800

    Update MasterMind.rb
    with ai guss (incomplete)

--- MasterMind.rb
+++ MasterMind.rb
@@ -3,7 +3,7 @@ class Player
 end
 
 class CreateScert
-    attr_reader :scert_color
+    attr_reader :scert_color, :all_color
     def initialize
         @scert_color = []
         4.times do
@@ -13,7 +13,7 @@ class CreateScert
     end
     
     def color
-        all_color = ["red","green","yellow","blue","white","black","grey","gold"]
+        @all_color = ["red","green","yellow","blue","white","black","grey","gold"]
     end
 end
 
@@ -22,6 +22,40 @@ class ScertArray
     def initialize(enter_guss = CreateScert.new.scert_color)
         @scert_code = enter_guss
     end   
+end
+
+class AiGuss
+    def initialize
+        ai_check
+        @last_correct = 0
+        @last_have = 0
+    end
+    
+    def ai_check (correct = 0, have = 0)
+        @correct = correct
+        @have = have
+    
+        if (correct == 0 && have == 0)
+            @ai_guss = new_guss
+        else if (@correct != 0)
+            
+            
+        end
+    end
+    
+    def new_guss
+        available = CreateScert.new.all_color
+        guss = []
+        4.times do
+            guss.push(available[rand(7)])
+        end
+        return guss
+    end
+    
+    def get_guss
+        @ai_guss.join(" ")
+    end
+    
 end
 
 class Game
@@ -60,5 +94,6 @@ end
 x = ScertArray.new
 p x.scert_code
 y = Game.new(x.scert_code)
-y.check(gets.chomp)
+z = AiGuss.new
+y.check(z.get_guss)
 puts y.correct,y.have
